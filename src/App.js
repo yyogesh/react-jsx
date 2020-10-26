bed6d11ad13b96122c68c13bb3eccc052ef90ccd
yyogesh <yogeshggn1@gmail.com>
2020-10-26 19:13:17 +0530

    React JSX

--- src/App.js
+++ src/App.js
@@ -1,22 +1,86 @@
-import logo from './logo.svg';
 import './App.css';
 
+
+// Wrong! This is a component and should have been capitalized:
+function Hello(props) {
+  console.log(props);
+  // Correct! This use of <div> is legitimate because div is a valid HTML tag:
+  return <div>Hello {props.toWhat + ' ' + props.toWho}</div>;
+}
+
+
+function HelloWorld() {
+  // Wrong! React thinks <hello /> is an HTML tag because it's not capitalized:
+  return <Hello toWhat="World" toWho="JavaScript" />;
+}
+
+
 function App() {
+  let count = 0;
+  const user = {
+    name: 'ravi',
+    age: 26,
+    location: 'delhi'
+  }
+  var badString = <script>alert('your site has been hacked')</script> // XSS 
+  const getLocation = (location) => {
+    return location || 'Unknown';
+    // return location ? location : 'Unknown';
+  }
+  const getLocation1 = (location) => {
+    if (location) {
+      return <li>User Location : {location}</li>
+    } else {
+      return <li>Location does not exist.</li>
+    }
+
+    // return location ? location : 'Unknown';
+  }
+  const addOne = () => {
+    count++;
+    console.log("addOne");
+    template();
+  };
+  const minusOne = () => {
+    count--;
+    console.log("addOne");
+    template()
+  };
+  const reset = () => {
+    count = 0;
+    console.log("addOne");
+    template()
+  };
+
+  const template = () => {
+    return (<div>
+      <h1>Count : {count}</h1>
+      <button onClick={addOne}>+1</button>
+      <button onClick={minusOne}>-1</button>
+      <button onClick={reset}>reset</button>
+    </div>)
+  }
+
+
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ul className="custom-class">
+          <li>User Name : {user.name}</li>
+          <li tabIndex={user.age}>User Age : {user.age}</li>
+          <li>User Location : {'New ' + user.location}</li>
+          <li>User Location : {['India', 'UK', 'USA'][2]}</li>
+          {getLocation1(user.location)}
+          {getLocation1('')}
+          {badString}
+          <li>{
+            user.age && user.age >= 18 && <span>You are eligible for vote. Your age is {user.age}</span>
+          }</li>
+        </ul>
+        {template()}
+        <HelloWorld />
       </header>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -11,6 +11,17 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+// setInterval(() => {
+//   const element = (
+//     <div>
+//       <h1>Hello, world!</h1>
+//       <h2>It is {new Date().toLocaleTimeString()}.</h2>
+//     </div>
+//   );
+//   ReactDOM.render(element, document.getElementById('root'));
+// }, 1000);
+
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
